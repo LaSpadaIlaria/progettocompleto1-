eed1253aa00ce95eb6342ef461b4b2a2e17eb978
LaSpadaIlaria <ilalaspada@gmail.com>
2026-04-14 18:27:29 +0200

    struttura nel file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+    console.log('Hello world!');
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
@@ -9,3 +14,5 @@
         color: aqua;
     }
 </style>
+
+<!-- questa è la struttura classica di un file svelte, abbiamo script quindi js per primo, poi abbiamo html e infine css -->
