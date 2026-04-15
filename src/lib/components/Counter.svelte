3faf2fdb01283c6730f23093170585f12074bf5e
LaSpadaIlaria <ilalaspada@gmail.com>
2026-04-15 10:19:14 +0200

    costoumize component con props e metto del counter

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { json } from "@sveltejs/kit";
+
+    let props = $props();
+    // con proprs andiamo ad accedere a un oggetto di js e poi lo sostituiamo sotto al posto dello 0
+    let count = $state(props.initialCount);
+    let doubled = $derived(count * 2);
+</script>
+
+<button on:click={() => count++}> 
+    Count: {count}
+</button>
+
+<span> doubled is {doubled}</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,30 @@
-<script>
+
+
+<!--<script>
     let count = $state(0);
     let doubled = $derived(count * 2);
-</script>
+</script>-->
 <!-- 2-abbiamo fatto questa cosa di creare una variabile e di far partire il contatore da 0. allora creo un button nel 3-->
  <!-- 4-faccio anche la variabile doubled per il bottone che raddoppia -->
+
+ <script>
+    import Counter from '$lib/components/Counter.svelte';
+ </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <!-- 3-creiamo un bottone in cui gli diciamo che al click deve eseguire questa funzione in cui fa +1. viene preso il valore di count da sopra e count viene sostituito da qui -->
-<button on:click={() => count++}>
+<!-- <button on:click={() => count++}> 
     Count: {count}
-</button>
+</button>-->
 
-
+<Counter initialCount={0} />
 
 <!-- 5-creiamo un bottone in cui gli diciamo che al click deve eseguire questa funzione in cui raddoppia il valore di count. viene preso il valore di doubled da sopra e doubled viene sostituito da qui -->
-<span> doubled is {doubled}</span>
+<!-- <span> doubled is {doubled}</span> -->
 
 <!-- questa è la struttura classica di un file svelte, abbiamo script quindi js per primo, poi abbiamo html e infine css -->
+
+
+<!-- 6-adesso ho creato nella libreria un connterintore in modo da creare un componente, posso spostare tutto quello del bottone sul contenitore. e poi semplicemente qui lo richiamo nello script-->
